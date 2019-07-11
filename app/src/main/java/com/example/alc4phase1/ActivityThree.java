42657d08fc70911521412c2e7989a9dcaa13e85b
nerojust <nerojust4@gmail.com>
2019-07-11 17:26:46 +0100

    few fixes.

--- app/src/main/java/com/example/alc4phase1/ActivityThree.java
+++ app/src/main/java/com/example/alc4phase1/ActivityThree.java
@@ -1,14 +1,32 @@
 package com.example.alc4phase1;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Objects;
 
 public class ActivityThree extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("My Profile");
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Respond to the action bar's Up/Home button
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/alc4phase1/ActivityTwo.java
+++ app/src/main/java/com/example/alc4phase1/ActivityTwo.java
@@ -10,8 +10,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+/**
+ * Handles the webview and refresh functionality.
+ */
 public class ActivityTwo extends AppCompatActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +27,58 @@ public class ActivityTwo extends AppCompatActivity {
         initializeViews();
     }
 
+    /**
+     * initializing all views
+     */
     @SuppressLint("SetJavaScriptEnabled")
     private void initializeViews() {
-        WebView alcWebsiteView = findViewById(R.id.webviewAlc);
+        final WebView alcWebsiteView = findViewById(R.id.webviewAlc);
         alcWebsiteView.getSettings().setJavaScriptEnabled(true);
         alcWebsiteView.setWebViewClient(new ALCwebviewClient());
         alcWebsiteView.loadUrl(getResources().getString(R.string.andela_website));
+        alcWebsiteView.canGoBack();
         alcWebsiteView.requestFocus();
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                alcWebsiteView.reload();
+            }
+        });
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+
+    }
+
+    /**
+     * Creating a Webview Client class for implementation.
+     */
     private class ALCwebviewClient extends WebViewClient {
-
-        private ProgressDialog dialog;
-
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            //in case there is ssl restriction by webview.
             handler.proceed();
         }
 
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            //show loading dialog
             dialog = ProgressDialog.show(ActivityTwo.this, "",
                     getResources().getString(R.string.loading), true);
+            //display refreshing at the top of screen
+            swipeRefreshLayout.setRefreshing(true);
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
+            //remove refreshing icon at top of screen
+            swipeRefreshLayout.setRefreshing(false);
+            //dismiss loading dialog
             dialog.dismiss();
         }
     }
